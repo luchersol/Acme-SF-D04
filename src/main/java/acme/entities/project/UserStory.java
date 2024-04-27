add1012418674d05445b13ae912a711fb534834a
Lucas Manuel Herencia Solís <93008812+luchersol@users.noreply.github.com>
2024-04-27 18:29:05 +0200

    Initialize Task2-E3

---
+++ src/main/java/acme/entities/project/UserStory.java
@@ -0,0 +1,62 @@
+
+package acme.entities.project;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import acme.datatypes.Priority;
+import acme.roles.Manager;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class UserStory extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Length(max = 75)
+	private String				title;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				description;
+
+	@Positive
+	private int					estimatedCost;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				acceptanceCriteria;
+
+	@URL
+	private String				link;
+
+	@NotNull
+	private Priority			priority;
+
+	@NotNull
+	@Valid
+	@ManyToOne
+	private Manager				manager;
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private Project				project;
+
+}
