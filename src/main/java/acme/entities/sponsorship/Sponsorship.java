a5e3ce2a690185d8a9677e04e987151e22a75cba
Lucas Manuel Herencia Solís <93008812+luchersol@users.noreply.github.com>
2024-04-27 18:29:20 +0200

    Initialize Task16-E3

---
+++ src/main/java/acme/entities/sponsorship/Sponsorship.java
@@ -0,0 +1,74 @@
+
+package acme.entities.sponsorship;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import acme.client.data.datatypes.Money;
+import acme.entities.project.Project;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Sponsorship extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "[A-Z]{1,3}-[0-9]{3}")
+	private String				code;
+
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	private Date				moment;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	private Date				startDate;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				endDate;
+
+	@NotNull
+	@Valid
+	private Money				amount;
+
+	@NotNull
+	private TypeOfSponsorship	type;
+
+	@Email
+	private String				email;
+
+	@URL
+	private String				link;
+
+	// Relationships -------------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private Project				project;
+
+}
