9952c642a8518d82258deb9cc76fee93e9a734c3
Lucas Manuel Herencia Solís <93008812+luchersol@users.noreply.github.com>
2024-04-27 18:29:26 +0200

    Initialize Task19-E1

---
+++ src/main/java/acme/entities/risk/Risk.java
@@ -0,0 +1,68 @@
+
+package acme.entities.risk;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import acme.client.data.accounts.Administrator;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Risk extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "R-[0-9]{3}")
+	private String				reference;
+
+	@NotNull
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				identificationDate;
+
+	@NotNull
+	@Positive
+	private Double				impact;
+
+	@NotNull
+	@Positive
+	private Double				probability;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				description;
+
+	@URL
+	private String				link;
+
+
+	@Transient
+	public Double getValue() {
+		return this.probability * this.impact;
+	}
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private Administrator administrator;
+
+}
