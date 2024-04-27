bfb076ede42adb78c186ecedc0e91de57f9f3c31
Lucas Manuel Herencia Solís <93008812+luchersol@users.noreply.github.com>
2024-04-27 18:29:05 +0200

    Initialize Task2-E2

---
+++ src/main/java/acme/entities/training/TrainingSession.java
@@ -0,0 +1,54 @@
+
+package acme.entities.training;
+
+import java.time.Period;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class TrainingSession extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "TS-[A-Z]{1,3}-[0-9]{3}")
+	private String				code;
+
+	private Period				timePeriod;
+
+	@NotBlank
+	@Length(max = 76)
+	private String				location;
+
+	@NotBlank
+	@Length(max = 76)
+	private String				instructor;
+
+	@NotBlank
+	@Email
+	private String				mandatoryContactEmail;
+
+	@URL
+	private String				furtherInformationLink;
+
+	@NotNull
+	@ManyToOne
+	private TrainingModule		trainingModule;
+
+}
