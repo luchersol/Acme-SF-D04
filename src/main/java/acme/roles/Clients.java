7e3b7e2b9ac1f535349f4b4fe8aec243ead49dd7
Lucas Manuel Herencia Solís <93008812+luchersol@users.noreply.github.com>
2024-04-27 18:29:15 +0200

    Initialize Task4-G

---
+++ src/main/java/acme/roles/Clients.java
@@ -0,0 +1,45 @@
+
+package acme.roles;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractRole;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+
+public class Clients extends AbstractRole {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "CLI-[0-9]{4}")
+	private String				identification;
+
+	@NotBlank
+	@Length(max = 75)
+	private String				companyName;
+
+	@NotNull
+	private TypeClients			type;
+
+	@NotBlank
+	@Email
+	private String				email;
+
+	@URL
+	private String				link;
+
+}
