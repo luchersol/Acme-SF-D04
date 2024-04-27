003685024c1df0fb84b8f0d40392bd43a76744cf
Lucas Manuel Herencia Solís <93008812+luchersol@users.noreply.github.com>
2024-04-27 18:29:09 +0200

    Initialize Task7-G

---
+++ src/main/java/acme/entities/audits/AuditRecord.java
@@ -0,0 +1,54 @@
+
+package acme.entities.audits;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class AuditRecord extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "AU-[0-9]{4}-[0-9]{3}")
+	private String				code;
+
+	@Past
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				startDate;
+
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				endDate;
+
+	@NotNull
+	private Mark				mark;
+
+	@URL
+	private String				link;
+
+	@ManyToOne
+	@Valid
+	private CodeAudit			codeAudit;
+
+}
---
+++ src/main/java/acme/entities/contract/ProgressLogs.java
@@ -0,0 +1,54 @@
+
+package acme.entities.contract;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Positive;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+public class ProgressLogs extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Column(unique = true)
+	@NotBlank
+	@Pattern(regexp = "PG-[A-Z]{1,2}-[0-9]{4}")
+	private String				recordId;
+
+	@Positive
+	@NotNull
+	private Integer				completeness;
+
+	@NotBlank
+	@Length(max = 100)
+	private String				comment;
+
+	@NotNull
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date				registrationMoment;
+
+	@NotBlank
+	@Length(max = 75)
+	private String				responsiblePerson;
+
+	@NotNull
+	@ManyToOne
+	private Contract			contract;
+
+}
