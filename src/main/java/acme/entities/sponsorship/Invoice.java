69394bc553c721614ef265f6bd6cdd6cee4ddd57
Lucas Manuel Herencia Solís <93008812+luchersol@users.noreply.github.com>
2024-04-27 18:29:21 +0200

    Initialize Task6-E2

---
+++ src/main/java/acme/entities/sponsorship/Invoice.java
@@ -0,0 +1,85 @@
+
+package acme.entities.sponsorship;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.PositiveOrZero;
+
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import acme.client.data.datatypes.Money;
+import acme.roles.Sponsor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Invoice extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "IN-[0-9]{4}-[0-9]{4}")
+	private String				code;
+
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	private Date				registrationTime;
+
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	private Date				dueDate;
+
+	@NotNull
+	private Money				quantity;
+
+	@PositiveOrZero
+	@NotNull
+	private Double				tax;
+
+	@URL
+	private String				link;
+
+	// Relationships -------------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private Sponsorship			sponsorship;
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	private Sponsor				sponsor;
+
+	// Derived attributes -------------------------------------------------------------
+
+
+	public Money totalAmount() {
+		Double finalAmount = this.quantity.getAmount() + this.tax / 100 * this.quantity.getAmount();
+		Money finalMoney = new Money();
+		finalMoney.setAmount(finalAmount);
+		finalMoney.setCurrency(this.quantity.getCurrency());
+		return finalMoney;
+	}
+
+}
