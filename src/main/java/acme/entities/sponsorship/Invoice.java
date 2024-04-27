399d772809d705f78e9534ce8b6f78975e1e690d
Lucas Manuel Herencia Solís <93008812+luchersol@users.noreply.github.com>
2024-04-27 18:29:13 +0200

    Initialize Task14-E5

---
+++ src/main/java/acme/entities/sponsorship/Invoice.java
@@ -0,0 +1,63 @@
+
+package acme.entities.sponsorship;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.PositiveOrZero;
+
+import org.hibernate.validator.constraints.URL;
+
+import acme.client.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Invoice extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	private static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@NotBlank
+	@Column(unique = true)
+	@Pattern(regexp = "IN-[0-9]{4}-[0-9]{4}")
+	private String				code;
+
+	@Past
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	private Date				registrationTime;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@NotNull
+	private Date				date;
+
+	@PositiveOrZero
+	private int					quantity;
+
+	@PositiveOrZero
+	private double				tax;
+
+	@URL
+	private String				link;
+
+	// Derived attributes -------------------------------------------------------------
+
+
+	public double totalAmount() {
+		return this.quantity + this.tax / 100 * this.quantity;
+	}
+
+}
